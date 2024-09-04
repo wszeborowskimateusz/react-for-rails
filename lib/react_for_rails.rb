5819e5a8e341b459d468b09c0e5c58635a8b1ea4
Mateusz Wszeborowski <wszeborowskimateusz@gmail.com>
2024-09-04 16:41:11 -0400

    Initial commit

---
+++ lib/react_for_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "react_for_rails/version"
+
+module ReactForRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/react_for_rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReactForRails do
+  it "has a version number" do
+    expect(ReactForRails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
